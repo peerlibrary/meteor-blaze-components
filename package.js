7e9d160ea8f41a9edd57d8b4ed68374e7c74431c
Mitar <mitar.git@tnode.com>
2016-08-02 17:04:59 -0700

    Updated dependency.

--- package.js
+++ package.js
@@ -60,7 +60,7 @@ Package.onUse(function (api) {
   api.use([
     'peerlibrary:assert@0.2.5',
     'peerlibrary:reactive-field@0.1.0',
-    'peerlibrary:computed-field@0.3.0',
+    'peerlibrary:computed-field@0.3.1',
     'peerlibrary:data-lookup@0.1.0'
   ]);
 
